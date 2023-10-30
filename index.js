d79ecf44c3ab13b50a4e70f2a7e046e3ddf20405
Arjun Sharma <arjunsharma6622@gmail.com>
2023-10-30 21:29:39 +0530

    added the PATCH to the cors

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cors({
     origin: ['https://aresuno.vercel.app', 'http://localhost:5173'],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Added OPTIONS method
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'], // Added OPTIONS method
 }));
 app.use(cookieParser());
 // ss
