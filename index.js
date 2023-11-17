32947000999a9a4a27740f36930845e11943be1b
Arjun Sharma <arjunsharma6622@gmail.com>
2023-11-17 22:59:00 +0530

    added image upload function in posts

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ const port = process.env.PORT;
 app.use(express.json());
 
 
+app.use(bodyParser.json({limit: '50mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+app.use(bodyParser.text({ limit: '200mb' }));
+
+
 // Configure Multer to handle file uploads
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
