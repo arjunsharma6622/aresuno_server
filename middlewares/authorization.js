8981ea71dcfe632edabf7098ca0f9be871a04d36
unknown <abhishekmaurya676@gmail.com>
2024-03-28 22:40:34 +0530

    neeraj testing server

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const Vendor = require("../models/Vendor");
 
+
+
 module.exports.verification = (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
     console.log("Token: in auth middlware", token);
