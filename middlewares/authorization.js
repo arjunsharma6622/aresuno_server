632db99e3b03a4b2949d1d65eadcf3f94b8882c0
Prashant <prashantrahul141@protonmail.com>
2024-04-09 23:22:23 +0530

    add auth headers check in verification middleware

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -5,6 +5,10 @@ const jwt = require("jsonwebtoken");
 const Vendor = require("../models/Vendor");
 
 module.exports.verification = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
   const token = req.headers.authorization.split(" ")[1];
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
