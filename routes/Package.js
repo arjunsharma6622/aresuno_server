679acae6fab22a4953caed69438a017bcf2d916a
Prashant <prashantrahul141@protonmail.com>
2024-04-13 15:45:13 +0530

    add: api routers for creating, updating, fetching package benefits

--- routes/Package.js
+++ routes/Package.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { verification, validateRole } = require("../middlewares/authorization");
 const logger = require("../utils/logger");
 const Package = require("../models/Package");
+const PackageBenefits = require("../models/PackageBenefits");
 
 // retrieve all price
 router.get("/getpackages", async (req, res) => {
@@ -50,5 +51,56 @@ router.post(
     }
   },
 );
+
+// create new package benefit
+router.post(
+  "/benefits/create",
+  verification,
+  validateRole(["admin"]),
+  async (req, res) => {
+    try {
+      const data = await PackageBenefits.insertMany(req.body);
+      res.status(201).json(data);
+    } catch (error) {
+      logger.error(error);
+      res.status(500).json({
+        error: `Failed to create new package benefits due to following error: ${error}`,
+      });
+    }
+  },
+);
+
+router.post(
+  "/benefits/update",
+  verification,
+  validateRole(["admin"]),
+  async (req, res) => {
+    try {
+      const data = await PackageBenefits.findByIdAndUpdate(
+        { _id: req.body._id },
+        req.body,
+      );
+      res.status(203).json(data);
+    } catch (error) {
+      logger.error(error);
+      res.status(500).json({
+        error: `Failed to update package benefits due to following error: ${error}`,
+      });
+    }
+  },
+);
+
+// fetch all benefits
+router.get("/benefits/fetchAll", async (req, res) => {
+  try {
+    const data = await PackageBenefits.find();
+    res.status(200).json(data);
+  } catch (error) {
+    logger.error(error);
+    res.status(500).json({
+      error: `Failed to retrieve all package benefits due to following error: ${error}`,
+    });
+  }
+});
 
 module.exports = router;
