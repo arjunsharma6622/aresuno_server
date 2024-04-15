814edff75aeef0c6ee4aea6878afa6aaf50ce16c
Prashant <prashantrahul141@protonmail.com>
2024-04-16 00:45:19 +0530

    remove routes related to PackageBenefits: we moved benefits to package itself

--- routes/Package.js
+++ routes/Package.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const { verification, validateRole } = require("../middlewares/authorization");
 const logger = require("../utils/logger");
 const Package = require("../models/Package");
-const PackageBenefits = require("../models/PackageBenefits");
 
 // retrieve all price
 router.get("/getpackages", async (req, res) => {
@@ -48,77 +47,6 @@ router.post(
     } catch (error) {
       logger.error(error);
       res.status(500).json({ error: "Failed to create categories" });
-    }
-  },
-);
-
-// create new package benefit
-router.post(
-  "/benefits/create",
-  verification,
-  validateRole(["admin"]),
-  async (req, res) => {
-    try {
-      const data = await PackageBenefits.insertMany(req.body);
-      res.status(201).json(data);
-    } catch (error) {
-      logger.error(error);
-      res.status(500).json({
-        error: `Failed to create new package benefits due to following error: ${error}`,
-      });
-    }
-  },
-);
-
-router.post(
-  "/benefits/update",
-  verification,
-  validateRole(["admin"]),
-  async (req, res) => {
-    try {
-      const data = await PackageBenefits.findByIdAndUpdate(
-        { _id: req.body._id },
-        req.body,
-      );
-      res.status(203).json(data);
-    } catch (error) {
-      logger.error(error);
-      res.status(500).json({
-        error: `Failed to update package benefits due to following error: ${error}`,
-      });
-    }
-  },
-);
-
-// fetch all benefits
-router.get("/benefits/fetchAll", async (req, res) => {
-  try {
-    const data = await PackageBenefits.find();
-    res.status(200).json(data);
-  } catch (error) {
-    logger.error(error);
-    res.status(500).json({
-      error: `Failed to retrieve all package benefits due to following error: ${error}`,
-    });
-  }
-});
-
-// delete a specific benefit
-router.delete(
-  "/benefits/delete/:benefitId",
-  verification,
-  validateRole(["admin"]),
-  async (req, res) => {
-    try {
-      const data = await PackageBenefits.findOneAndDelete({
-        _id: req.params.benefitId,
-      });
-      res.status(202).json(data);
-    } catch (error) {
-      logger.error(error);
-      res.status(500).json({
-        error: `Failed to delete benefit due to following error: ${error}`,
-      });
     }
   },
 );
